00a4f50335f606a77036e7138a8f166b668f54c4
Alex Otoo <alexotoous@outlook.com>
2021-04-05 20:10:00 +0000

    project first complete V1 done

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,19 @@
+import { Center } from "@chakra-ui/layout";
+
 export default function Footer() {
   return (
-    <div>
-      <h2>footer</h2>
-    </div>
+    <Center
+      bg="black"
+      marginTop="2rem"
+      color="gray.400"
+      padding="2rem"
+      fontWeight="bolder"
+      textAlign="center"
+    >
+      <div>
+        <h1 className="logo">Eatify</h1>
+        <a href="https://alexooodev.netlify.app/">Developed by AlexooO</a>
+      </div>
+    </Center>
   );
 }
